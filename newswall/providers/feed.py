eaec85a2dcfd37887d797d427d9c539338afdf14
J. Heasly <jheasly@earthlink.net>
2014-06-19 20:01:41 -0700

    We have good timestamps!
    ... so far.

--- newswall/providers/feed.py
+++ newswall/providers/feed.py
@@ -13,6 +13,7 @@ from datetime import datetime
 import feedparser
 import pytz
 import time
+from email.utils import parsedate
 
 from newswall.providers.base import ProviderBase
 
@@ -25,12 +26,14 @@ class Provider(ProviderBase):
                 timestamp = datetime.fromtimestamp(
                     time.mktime(entry.date_parsed))
             elif hasattr(entry, 'published_parsed'):
-                timestamp = datetime.fromtimestamp(
-                    time.mktime(entry.published_parsed[:8] + (-1,)))
+#                 timestamp = datetime.fromtimestamp(
+#                     time.mktime(entry.published_parsed[:8] + (-1,)))
+                
+                timestamp = datetime(*(parsedate(entry.published)[:6]))
             else:
                 timestamp = datetime.now()
             
-            timestamp = timestamp.replace(tzinfo=pytz.utc)
+#             timestamp = timestamp.replace(tzinfo=pytz.utc)
             
             self.create_story(
                 entry.link,
